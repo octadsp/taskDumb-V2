a00f72d7122e568e34eb815e3ebfcd68e4860582
tole <octaadsp.camp@gmail.com>
2023-03-10 02:43:27 +0700

    Day12 -EditButton

--- main.go
+++ main.go
@@ -5,12 +5,27 @@ import (
 	"html/template"
 	"io"
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 )
 
 type Template struct {
 	templates *template.Template //mengirim request
+}
+
+type Project struct {
+	Projectname string
+	Duration    string
+	Description string
+}
+
+var dataProject = []Project{
+	{
+		Projectname: "Ini Project TESTING",
+		Duration:    "5 Bulan",
+		Description: "Ini Project TESTING",
+	},
 }
 
 // Function Render = pada function ini mendeklarasikan alias t dengan template(dari package Template echo)
@@ -32,11 +47,15 @@ func main() {
 	e.Renderer = t
 
 	// Routing
-	e.GET("/hello", helloWorld)       //to execute //localhost:5000/hello
-	e.GET("/", home)                  //localhost:5000
-	e.GET("/contact", contact)        //localhost:5000/contact
-	e.GET("/myProject", myproject)    //localhost:5000/myProject
-	e.POST("/myProject", postproject) //localhost:5000/myProject
+	e.GET("/hello", helloWorld)                //to execute //localhost:5000/hello
+	e.GET("/", home)                           //localhost:5000
+	e.GET("/contact", contact)                 //localhost:5000/contact
+	e.GET("/myProject", myproject)             //localhost:5000/myProject
+	e.GET("/detailProject/:id", detailproject) //localhost:5000/detailProject/:id
+	e.POST("/myProject", postproject)          //localhost:5000/myProject
+	e.GET("/deleteProject/:id", deleteproject)
+	e.GET("/editProject/:id", editproject) //localhost:5000/deleteProject
+	// e.POST("/editProject/:id", postEditProject) //localhost:5000/deleteProject
 
 	fmt.Println("Server Berjalan di port 5000")
 	e.Logger.Fatal(e.Start("localhost:5000"))
@@ -48,7 +67,11 @@ func helloWorld(c echo.Context) error {
 
 // Function Home
 func home(c echo.Context) error {
-	return c.Render(http.StatusOK, "index.html", nil)
+	projects := map[string]interface{}{
+		"Projects": dataProject,
+	}
+
+	return c.Render(http.StatusOK, "index.html", projects)
 }
 
 // Function Contact
@@ -63,22 +86,87 @@ func myproject(c echo.Context) error {
 
 func postproject(c echo.Context) error {
 	projectName := c.FormValue("projectName")
-	startDate := c.FormValue("startDate")
-	endDate := c.FormValue("endDate")
+	// startDate := c.FormValue("startDate")
+	// endDate := c.FormValue("endDate")
 	description := c.FormValue("description")
-	// tech1 := c.FormValue("tech1")
-	// tech2 := c.FormValue("tech2")
-	// tech3 := c.FormValue("tech3")
-	// tech4 := c.FormValue("tech4")
 
-	println("Project Name : " + projectName)
-	println("Start Date : " + startDate)
-	println("End Date : " + endDate)
-	println("Description : " + description)
-	// println("Technology : " + tech1)
-	// println("Technology : " + tech2)
-	// println("Technology : " + tech3)
-	// println("Technology : " + tech4)
+	var newProject = Project{
+		Projectname: projectName,
+		Duration:    "9 Bulan Ges",
+		Description: description,
+	}
+
+	dataProject = append(dataProject, newProject)
 
 	return c.Redirect(http.StatusMovedPermanently, "/")
 }
+
+func detailproject(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+
+	var ProjectDetail = Project{}
+
+	for i, data := range dataProject {
+		if id == i {
+			ProjectDetail = Project{
+				Projectname: data.Projectname,
+				Duration:    data.Duration,
+				Description: data.Description,
+			}
+		}
+	}
+
+	//data yang akan dikirimkan ke html menggunakan map interface
+	detailProject := map[string]interface{}{
+		"Project": ProjectDetail,
+	}
+
+	return c.Render(http.StatusOK, "detailProject.html", detailProject)
+}
+
+func deleteproject(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+
+	dataProject = append(dataProject[:id], dataProject[id+1:]...)
+
+	return c.Redirect(http.StatusMovedPermanently, "/")
+}
+
+func editproject(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+
+	var ProjectEdit = Project{}
+
+	for i, data := range dataProject {
+		if id == i {
+			ProjectEdit = Project{
+				Projectname: data.Projectname,
+				Duration:    data.Duration,
+				Description: data.Description,
+			}
+		}
+	}
+
+	editProject := map[string]interface{}{
+		"Project": ProjectEdit,
+	}
+
+	return c.Render(http.StatusOK, "editProject.html", editProject)
+}
+
+// func postEditProject(c echo.Context) error {
+// 	id, _ := strconv.Atoi(c.Param("id"))
+
+// 	projectNameEdit := c.FormValue("projectNameEdit")
+// 	descriptionEdit := c.FormValue("descriptionEdit")
+
+// 	newEditedProject := Project{
+// 		Projectname: projectNameEdit,
+// 		Duration:    "9 Bulan Ges",
+// 		Description: descriptionEdit,
+// 	}
+
+// 	dataProject[id] = append(dataProject, newEditedProject )
+
+// 	return c.Redirect(http.StatusMovedPermanently, "/")
+// }
